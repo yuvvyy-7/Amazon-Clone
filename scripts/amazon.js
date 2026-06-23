0c1a9e975b17361b0f60d0272f4d3c4431953cd3
yuvraj <yuvvyy7@gmail.com>
2026-06-23 23:09:36 +0530

    added some extra functions and started checkout.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utility/money.js';
 
 let productsHTML = '';
 
@@ -23,7 +24,7 @@ products.forEach((products) => {
           </div>
 
           <div class="product-price">
-            $${(products.priceCents / 100).toFixed(2)}
+            $${formatCurrency(products.priceCents)}
           </div>
 
           <div class="product-quantity-container">
---
+++ scripts/checkout.js
@@ -0,0 +1,3 @@
+import { formatCurrency } from "./utility/money";
+import { products } from "../data/products";
+import { cart } from "../data/cart";
