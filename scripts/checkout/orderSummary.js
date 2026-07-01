b3a3b44f83150af293a116ccfbc757d750fa3833
yuvraj <yuvvyy7@gmail.com>
2026-07-02 02:25:40 +0530

    added renderOrderSummary Test using jasmine framework

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -25,7 +25,9 @@ cart.forEach((cartItem) => {
     const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
-    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    <div class="cart-item-container 
+                js-cart-item-container 
+                js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: ${dateString}
             </div>
@@ -41,7 +43,7 @@ cart.forEach((cartItem) => {
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
@@ -51,7 +53,11 @@ cart.forEach((cartItem) => {
                   <input type="number" class="quantity-input"> 
                   <span class="save-quantity-link link-primary" data-product-id="${matchingProduct.id}">Save</span>
 
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                  <span class="delete-quantity-link 
+                               link-primary 
+                               js-delete-link 
+                               js-delete-link-${matchingProduct.id}" 
+                               data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
