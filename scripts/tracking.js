b0808fa24fb79e0b9b99dfea3ef18e8cbc811c5a
yuvraj <yuvvyy7@gmail.com>
2026-07-06 17:50:54 +0530

    made tracking bar interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -22,9 +22,15 @@ async function loadPage() {
             productDetails = details;
         }
     });
-
-    updateCartQuantity();
     
+    updateCartQuantity();
+
+    const today = dayjs();
+    const orderTime = dayjs(order.orderTime);
+    const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+    
+    const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
     const trackingHTML = `
    
       <div class="order-tracking">
@@ -49,19 +55,24 @@ async function loadPage() {
         <img class="product-image" src="${product.image}">
 
         <div class="progress-labels-container">
-          <div class="progress-label">
+          <div class="progress-label ${
+        percentProgress < 50 ? 'current-status' : ''
+      }">
             Preparing
           </div>
-          <div class="progress-label current-status">
+          <div class="progress-label ${
+        (percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''}">
             Shipped
           </div>
-          <div class="progress-label">
+          <div class="progress-label ${
+            percentProgress >= 100 ? 'current-status' : ''
+          }">
             Delivered
           </div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="progress-bar" style="width: ${percentProgress}%;"></div>
         </div>
       </div>
 
